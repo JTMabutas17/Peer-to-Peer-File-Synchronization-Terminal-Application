2a8362b66382150c4087b05ce705d7e247671b10
Joseph Cuevas <josephcuevas77@outlook.com>
2021-04-29 12:25:37 -0700

    added try catch

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -12,7 +12,7 @@ Authors: Justin Mabutas and Joseph Cuevas
 def ping_network():
     host_ip = socket.gethostbyname(socket.gethostname())
     # Get the network from the host_ip with regex.
-    network = re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\b.", host_ip).group()
+    network = re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\b", host_ip).group()
     for num in range(1,255):
         # Use Popen to asynchronously run ping commands on all hosts in the current network from [1,255). Ignore output.
         subprocess.Popen("ping -n 1 " + network + str(num), shell=True, stdout=False)
@@ -23,7 +23,7 @@ def get_ip_addresses():
     host_ip = socket.gethostbyname(socket.gethostname())
     gateway_ip = "1"
     broadcast_ip = "255"
-    network = re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\b.", host_ip).group()
+    network = re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\b", host_ip).group()
     # Save output of arp -a to ip_addresses
     ip_addresses = subprocess.check_output("arp -a", shell=True)
     # Currently ip_addresses is a bytes-like object, so decode it to utf-8
@@ -34,11 +34,15 @@ def get_ip_addresses():
     # Define ip_to_ignore to include host_ip, gateway_ip, and broadcast_ip
     ip_to_ignore = [host_ip, gateway_ip, broadcast_ip]
     # Use regex to find all ip addresses that match network
-    ip_addresses = re.findall(network+".\d{1,3}", ip_addresses)
+    ip_addresses = re.findall(network+"\d{1,3}", ip_addresses)
 
+    print(ip_addresses)
     # Remove all elements in ip_to_ignore
-    for ip in ip_to_ignore:
-        ip_addresses.remove(ip)
+    for x in ip_to_ignore:
+        try:
+            ip_addresses.remove(x)
+        except ValueError:
+            print("Error Caught, Value not present int list: " + x)
     return ip_addresses
 
 if __name__ == '__main__':
